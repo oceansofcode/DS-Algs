8107212f12c8cb0d623d8085069348dc3154d590
Adam <adam.adj@protonmail.com>
2020-05-27 10:11:35 -0400

    Only using rust for DS Algs now

---
+++ guessing_game/src/main.rs
@@ -0,0 +1,27 @@
+use std::io;
+use rand::Rng;
+use std::cmp::Ordering;
+
+fn main() {
+    println!("Guess the Number!");
+
+    let secret_number = rand::thread_rng().gen_range(1, 101);
+
+    println!("The secret number is: {}", secret_number);
+
+    println!("Please input your guess.");
+
+    let mut guess = String::new();
+
+    io::stdin().read_line(&mut guess).expect("Failed to read line");
+
+    let guess: u8 = guess.trim().parse().expect("Please type a number");
+
+    println!("You guess: {}", guess);
+
+    match guess.cmp(&secret_number) {
+        Ordering::Less => println!("Too small!"),
+        Ordering::Greater => println!("Too big!"),
+        Ordering::Equal => println!("You win!"),
+    }
+}
